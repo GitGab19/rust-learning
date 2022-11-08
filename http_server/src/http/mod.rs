9a53b50e530e980dc024a9e5017f073f5636477a
GitGab19 <gabriele.vernetti@protonmail.com>
2022-11-08 11:59:39 +0100

    updates

--- http_server/src/http/mod.rs
+++ http_server/src/http/mod.rs
@@ -1,5 +1,9 @@
 pub use request::Request;
 pub use method::Method;
 pub use request::ParseError;
+pub use query_string::{QueryString, Value as QueryStringValue};
 pub mod request;
+pub use response::Response;
 pub mod method;
+pub mod response;
+pub mod query_string;
--- http_server/src/http/request.rs
+++ http_server/src/http/request.rs
@@ -5,9 +5,12 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::str;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use super::{QueryString};
+
+#[derive(Debug)]
 pub struct Request<'buf> {
     path: &'buf str,
-    query_string: Option<&'buf str>,
+    query_string: Option<QueryString<'buf>>,
     method: Method,
 }
 
@@ -31,7 +34,7 @@ impl<'buf> TryFrom<&'buf[u8]> for Request<'buf> {
 
         let mut query_string = None;
         if let Some(i) = path.find('?') {
-            query_string = Some(&path[i + 1..]);
+            query_string = Some(QueryString::from(&path[i + 1..]));
             path = &path[..i];
         }
 
